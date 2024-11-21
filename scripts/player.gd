3d27acbae839cf17a07d5e44625f8608dbc794d2
Flinz <flinzwongjingewe@gmail.com>
2024-11-22 01:34:45 +0800

    Dakan stuff

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,6 +5,7 @@ func _ready():
 const SPEED = 500.0
 const JUMP_VELOCITY = -300.0
 
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.

93f8f234ce0789da23c4c0c7be0e729b579d3956
Vexcenot <yoshimasacyz@gmail.com>
2024-11-23 02:13:57 +0800

    added win animation

--- scripts/win_condition.gd
+++ scripts/win_condition.gd
@@ -3,10 +3,19 @@ extends Node2D
 	#$AudioStreamPlayer.play()
 	#await get_tree().create_timer(.2).timeout
 
+@onready var transition: AnimationPlayer = $Transition
+@onready var canvas_layer: CanvasLayer = $CanvasLayer
+@onready var color_rect: ColorRect = $CanvasLayer/ColorRect
+
+
 
 func _ready():
+	transition.play_backwards("fade")
+	await get_tree().create_timer(1).timeout
+	canvas_layer.free()
+	#animation_player.play("gone")
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-
+	
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
 	DragManager.is_dragging = false
--- scripts/winanimate.gd
+++ scripts/winanimate.gd
@@ -1,4 +1,5 @@
 extends Node2D
+var transition = preload("res://scenes/transition.tscn")
 
 func trans(a, b):
 	var speed = 0.5
@@ -8,8 +9,23 @@ func trans(a, b):
 	tween.tween_property(b, "modulate:a", 1.0, speed)
 	await get_tree().create_timer(delay).timeout
 
+func glow():
+	var tween = get_tree().create_tween()
+	tween.tween_property($WhiteGlow, "scale", Vector2(100,100), 50)
+	pass
+
+func flash():
+	var instance = transition.instantiate()
+	add_child(instance)
+
 func _ready():
 	global_position = get_global_mouse_position()
 	# Start the dialog transition
 	await trans($"1", $"2") # Adjust speed and delay as needed
 	await trans($"2", $"3") # Adjust speed and delay as needed
+	trans($"3", $"4")
+	$"5".visible = true
+	await get_tree().create_timer(1).timeout
+	glow()
+	await get_tree().create_timer(2).timeout
+	flash()

3b9785ab8e8f88a84171e1eccb86936a55119aec
Vexcenot <yoshimasacyz@gmail.com>
2024-11-22 20:55:16 +0800

    added win animation

---
+++ scripts/FrameManager.gd
@@ -0,0 +1,10 @@
+extends Node
+
+var fade1 = false
+var fade2 = false
+var fade3 = false
+var fade4 = false
+var fade5 = false
+var fade6 = false
+var fade7 = false
+var fade8 = false
--- winanimate.gd
+++ winanimate.gd
@@ -1,26 +1,15 @@
 extends Node2D
 
-var speed = 1
-var time = 40
-var tween := create_tween()
+func trans(a, b):
+	var speed = 0.5
+	var delay = 1.4
+	var tween := create_tween().set_parallel(true)
+	tween.tween_property(a, "modulate:a", 0.0, speed)
+	tween.tween_property(b, "modulate:a", 1.0, speed)
+	await get_tree().create_timer(delay).timeout
 
-func fuck():
-	tween.tween_property($"2", "modulate:a", 0.0, 0)
-	tween.tween_property($"3", "modulate:a", 0.0, 0)
-	tween.tween_property($"4", "modulate:a", 0.0, 0)
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	fuck()
+func _ready():
 	global_position = get_global_mouse_position()
-	tween.tween_property($"1", "modulate:a", 1.0, speed)
-	$"2".visible = true
-	tween.tween_property($"2", "modulate:a", 1.0, speed)
-	$"3".visible = true
-	tween.tween_property($"2", "modulate:a", 0.0, speed)
-	tween.tween_property($"1", "modulate:a", 0.0, speed)
-	tween.tween_property($"3", "modulate:a", 1.0, speed)
-
-#func _process(delta: float) -> void:
-	# Update the position of the scene to follow the cursor
-	
+	# Start the dialog transition
+	await trans($"1", $"2") # Adjust speed and delay as needed
+	await trans($"2", $"3") # Adjust speed and delay as needed
